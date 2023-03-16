fcc22975bad0d6b590c8ecfa4579904577e94ab4
MemeBraindt <IlyaBraindt@yandex.by>
2023-03-16 11:41:35 +0300

    bug fix 1st task

--- task_3_1.py
+++ task_3_1.py
@@ -1,7 +1,11 @@
 s = input("Введите строку: \n")
-s = s.split()
+s = s.split(" ")
 new_words = []
-for words in s:
-    if len(words) > 5:
-        new_words.append(words)
+repl = [".", ",", "?", "!"]
+for word in s:
+    for x in repl:
+        word = word.replace(x, "")
+    # print(word)
+    if len(word) > 5:
+        new_words.append(word)
 print(" ".join(new_words))
